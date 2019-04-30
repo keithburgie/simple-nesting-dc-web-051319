f3f9a8641b1d6a8feb2a8e97f3ee24c65a3077f6
Keith Burgie <krburgie@gmail.com>
2019-04-30 16:47:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,11 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
+ 		  }
 
     programmer_hash[:yukihiro_matsumoto]
 end
